4b327cc7ecc21d67fd81e71b483550cc05a99b2e
rckmath <ericklopes02@gmail.com>
2026-01-19 17:47:10 -0300

    Initial Commit

---
+++ ImmaDeploy/ImmaDeployApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImmaDeployApp.swift
+//  ImmaDeploy
+//
+//  Created by Erick Matheus on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ImmaDeployApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
